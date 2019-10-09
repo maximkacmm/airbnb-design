8a02c13088df135c8d8685a2c233159b5a597184
Maximka <maximkacmm@gmail.com>
2019-10-09 09:31:25 +0300

    created component Header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import ellipse from './ellipse.jpg';
-
 import './style.css';
+import Header from "./Header/Header";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header wrapper">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <div className="NavBar">
-                    <a href="#" className="NavBar-linc">Become a host</a>
-                    <a href="#" className="NavBar-linc">Trips</a>
-                    <a href="#" className="NavBar-linc">Trips</a>
-                    <a href="#" className="NavBar-linc linc__ellipse"><img src={ellipse} className="App-logo" alt="users logo"/></a>
-                </div>
-            </header>
+           <Header/>
         </div>
     );
 }
---
+++ src/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from "../logo.svg";
+import ellipse from "../ellipse.jpg";
+
+
+function Header() {
+    return (
+        <div className="App">
+            <header className="App-header wrapper">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <div className="NavBar">
+                    <a href="#" className="NavBar-linc">Become a host</a>
+                    <a href="#" className="NavBar-linc">Trips</a>
+                    <a href="#" className="NavBar-linc">Trips</a>
+                    <a href="#" className="NavBar-linc linc__ellipse"><img src={ellipse} className="App-logo" alt="users logo"/></a>
+                </div>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
