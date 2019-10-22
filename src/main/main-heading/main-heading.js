42fbf9b418b250752c3b1c73f662da209affb153
Maximka <maximkacmm@gmail.com>
2019-10-22 11:35:25 +0300

    full page second check

---
+++ src/main/main-heading/main-heading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './main-heading.css';
+
+function MainHeading () {
+    return (
+        <div className="main-heading wrapper-inner">
+             <h1 className="main-heading">
+                 Book unique homes and experiences all over the world.
+             </h1>
+        </div>
+    );
+}
+export default MainHeading;
